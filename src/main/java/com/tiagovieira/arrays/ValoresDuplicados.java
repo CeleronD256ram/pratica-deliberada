84b09b37b205b8300225c5cfb4a46d4ca7f3d0e9
CeleronD256ram <celedond256@outlook.com>
2024-11-29 15:04:56 -0300

    Tema: arrays, Ajusta um vetor e retorna um novo vetor sem números repetidos

--- src/main/java/com/tiagovieira/arrays/ValoresDuplicados.java
+++ src/main/java/com/tiagovieira/arrays/ValoresDuplicados.java
@@ -2,49 +2,43 @@ package com.tiagovieira.arrays;
 
 import java.util.Arrays;
 
-/*Dado um vetor de inteiros, remova os valores duplicados de forma que cada elemento
- apareça apenas uma vez. Retorne o vetor resultante ordenado.
- */
+/* Dado um vetor de inteiros, remova os valores duplicados de forma que cada elemento
+   apareça apenas uma vez. Retorne o vetor resultante ordenado. */
 public class ValoresDuplicados {
     public static void main(String[] args) {
-        int[] vetor = {10, 12, 10, 8, 4, 2, 7, 8, 2};
+
+        int[] vetor = {2, 2, 2, 2, 3, 4, 6, 87, 2, 1};
 
         Arrays.sort(vetor);
 
-        int[] resultado = removerDuplicados(vetor);
-
-        System.out.println(Arrays.toString(resultado));
-    }
-
-    public static int[] removerDuplicados(int[] vetor) {
-
         int tamanho = 1;
-        //Conta quantos números não repetidos tem no array
+        //Determina qual será o tamanho do novo array para alocar os número que não se repetem
         for (int i = 1; i < vetor.length; i++) {
             if (vetor[i] != vetor[i - 1]) {
                 tamanho++;
 
             }
-
-
         }
-        //Cria um novo array com o tamanho dos elementos não repetidos
-        int[] resultado = new int[tamanho];
-        resultado[0] = vetor[0]; // o primeiro elemento é sempre único
+
+        int[] vetorDeUnicos = new int[tamanho];
+        vetorDeUnicos[0] = vetor[0];
 
         int index = 1;
         for (int i = 1; i < vetor.length; i++) {
             if (vetor[i] != vetor[i - 1]) {
-                resultado[index++] = vetor[i];
+                vetorDeUnicos[index++] = vetor[i];
 
             }
+        }
+
+        System.out.println("Novo vetor sem número repetidos:");
+        for (int vetorDeUnico : vetorDeUnicos) {
+            System.out.print(vetorDeUnico + " ");
+
+        }
 
 
         }
-        return resultado;
-
-
-    }
 
 
 }
