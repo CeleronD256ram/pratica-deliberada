8c02d0331eeff350db2ef5041ee405465f578c0a
Tiago Vieira <145232497+CeleronD256ram@users.noreply.github.com>
2024-12-30 11:21:27 -0300

    Beecrowd: problema  1097 resolvido.

--- src/main/java/com/tiagovieira/beecrowd/Exe1097.java
+++ src/main/java/com/tiagovieira/beecrowd/Exe1097.java
@@ -1,4 +1,31 @@
 package com.tiagovieira.beecrowd;
 
 public class Exe1097 {
+    public static void main(String[] args) {
+
+
+        int i = 1;
+
+        int j = 7;
+
+        // Enquanto I for menor ou igual a 9, continue o loop
+        while (i <= 9) {
+            // Laço interno para imprimir os valores de J
+            for (int count = 0; count < 3; count++) {
+                // Imprime os valores atuais de I e J
+                System.out.println("I=" + i + " J=" + j);
+
+                // Decrementa J em 1 após cada impressão
+                j--;
+            }
+
+            i += 2;
+
+            // Reinicia o valor de J para o próximo bloco
+            j += 5;
+        }
+
+
+    }
+
 }
