0a3f60ecc40658c05e09407b35ef3573fc0c6d5f
Tiago Vieira <145232497+CeleronD256ram@users.noreply.github.com>
2025-02-01 23:33:30 -0300

    Refatoração na Busca Binária

--- src/main/java/com/tiagovieira/estruturasDeDados/BuscaBinaria.java
+++ src/main/java/com/tiagovieira/estruturasDeDados/BuscaBinaria.java
@@ -1,45 +1,44 @@
 package com.tiagovieira.estruturasDeDados;
 
+
+
 public class BuscaBinaria {
+
+    private static final int ARRAY_TAMANHO = 100;
+
     public static void main(String[] args) {
-        int[] array = new int[100];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i;
+        int[] array = inicializarArrayOrdenado(ARRAY_TAMANHO);
 
-        }
-        System.out.println(buscaBinaria(array, 38));
+        int elementoBuscado = 38;
+        int resultado = buscaBinaria(array, elementoBuscado);
 
-
+        System.out.println("Índice do elemento " + elementoBuscado + ": " + resultado);
     }
 
+    public static int[] inicializarArrayOrdenado(int tamanho) {
+        int[] array = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            array[i] = i;
+        }
+        return array;
+    }
 
     public static int buscaBinaria(int[] array, int elemento) {
         int menorIndice = 0;
         int maiorIndice = array.length - 1;
 
-        int meio = 0;
-        int chute = 0;
-
         while (menorIndice <= maiorIndice) {
-            meio = (menorIndice + maiorIndice) / 2;
-            chute = array[meio];
+            int meio = menorIndice + (maiorIndice - menorIndice) / 2;
 
-            if (chute == elemento) {
+            if (array[meio] == elemento) {
                 return meio;
-
-            } else if (chute > elemento) {
+            } else if (array[meio] > elemento) {
                 maiorIndice = meio - 1;
-
             } else {
                 menorIndice = meio + 1;
             }
-
-
         }
 
-        return  - 1;
-
-
+        return -1; // Elemento não encontrado
     }
-
 }
